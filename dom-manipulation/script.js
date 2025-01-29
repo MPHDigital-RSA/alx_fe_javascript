a7118caf563f03f20ff2aee173988a230c7b122b
Prince Hlela <mphdigital@protonmail.com>
2025-01-29 18:06:50 +0200

    index.html and script.js files created

---
+++ dom-manipulation/script.js
@@ -0,0 +1,47 @@
+const quoteDisplay = document.getElementById('quoteDisplay');
+const newQuote = document.getElementById('newQuote');
+
+const quotes = [
+    {
+        "text": ' "Quote 1"  ',
+        "category" : "science"
+    },
+
+    {
+        "text": ' "Quote 2"  ',
+        "category" : "soccer"
+    },
+
+    {
+        "text": ' "Quote 3"  ',
+        "category" : "science"
+    },
+
+    {
+        "text": ' "Quote 4"  ',
+        "category" : "tennis"
+    },
+
+    {
+        "text": ' "Quote 5"  ',
+        "category" : "soccer"
+    },
+
+    {
+        "text": ' "Quote 6"  ',
+        "category" : "tennis"
+    }
+];
+
+
+// console.log(randomQuoteIndex)
+
+
+function showRandomQuote  () {
+
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    
+    quoteDisplay.textContent = quotes[randomQuoteIndex].text;
+}
+
+newQuote.addEventListener ('click', showRandomQuote);
